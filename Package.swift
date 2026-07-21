b3f8b7ecdd8810109858f29d059be1e4a38455cb
Ivan Magda <imagda15@gmail.com>
2026-07-21 15:22:44 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,29 +3,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-speech-lanes",
-    platforms: [
-        .iOS(.v26),
-        .macOS(.v26),
-        .visionOS(.v26)
-    ],
-    products: [
-        .library(
-            name: "SpeechLanes",
-            targets: ["SpeechLanes"]
-        )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0")
-    ],
-    targets: [
-        .target(
-            name: "SpeechLanes"
-        ),
-        .testTarget(
-            name: "SpeechLanesTests",
-            dependencies: ["SpeechLanes"],
-            resources: [.copy("Fixtures")]
-        )
-    ]
+  name: "swift-speech-lanes",
+  platforms: [
+    .iOS(.v26),
+    .macOS(.v26),
+    .visionOS(.v26),
+  ],
+  products: [
+    .library(
+      name: "SpeechLanes",
+      targets: ["SpeechLanes"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0")
+  ],
+  targets: [
+    .target(
+      name: "SpeechLanes"
+    ),
+    .testTarget(
+      name: "SpeechLanesTests",
+      dependencies: ["SpeechLanes"],
+      resources: [.copy("Fixtures")]
+    ),
+  ]
 )
